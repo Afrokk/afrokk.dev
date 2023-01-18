195d20886891608b34b0bd2db3eb7ea90eb9a579
Afrokk <afrasiyabkhan379@gmail.com>
2023-01-17 20:47:22 -0500

    setup Adobe/Webpack modules

---
+++ adobe_config.js
@@ -0,0 +1,26 @@
+var __config__ = {
+  page_id: "p6346ba9fc772a7f7876af649354e05c9e7e2cbe9bd687de175cec",
+  theme: { name: "marta" },
+  pageTransition: true,
+  linkTransition: true,
+  disableDownload: true,
+  localizedValidationMessages: {
+    required: "This field is required",
+    Email: "This field must be a valid email address",
+  },
+  lightbox: { enabled: false },
+  cookie_banner: { enabled: false },
+};
+
+window.NREUM || (NREUM = {});
+NREUM.info = {
+  beacon: "bam.nr-data.net",
+  licenseKey: "e7fb1b89a0",
+  applicationID: "750147145",
+  transactionName: "ZwZaYkJVDERXUxULCV5Me0NDQA1aGWsmJzJtQxdtS0QDQ14KT0gbbQ==",
+  queueTime: 0,
+  applicationTime: 37,
+  atts: "S0FNFApPHxsUUUNYHU0e",
+  errorBeacon: "bam.nr-data.net",
+  agent: "",
+};
