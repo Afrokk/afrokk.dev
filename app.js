7140047cab34f3a6507191ada5796fa133737fed
Afrokk <afrasiyabkhan379@gmail.com>
2023-01-21 05:45:12 -0500

    Add pages

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ window.onpageshow = function (e) {
     window.location.reload();
   }
 };
+
+function checkFeat(funcString, featName) {
+  try {
+    var func = eval("self." + funcString);
+    -1 !== func.toString().indexOf("[native code]")
+      ? (featureStatus[featName] = Status.NATIVE)
+      : (featureStatus[featName] = Status.CHANGED);
+  } catch (e) {
+    featureStatus[featName] = Status.UNAVAIL;
+  }
+}
