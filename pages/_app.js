d59277ada529b7b5b8040111ba4a7a6f72b89dc4
Afrokk <afrasiyabkhan379@gmail.com>
2023-04-16 17:51:38 -0400

    update index and headers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globalStyles.sass'
+import Head from "next/head";
+import { Fragment, useEffect, useState } from "react";
+import PreLoader from "../src/layout/PreLoader";
+import "../styles/global.sass";
+import "../styles/base.sass";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Afrasiyab - Software Engineer</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      {load && <PreLoader />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
+
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="static/plugins/bootstrap/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link href="static/plugins/et-line/style.css" rel="stylesheet" />
+        <link
+          href="static/plugins/themify-icons/themify-icons.css"
+          rel="stylesheet"
+        />
+        <link
+          href="static/plugins/owl-carousel/css/owl.carousel.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="static/plugins/magnific/magnific-popup.css"
+          rel="stylesheet"
+        />
+        <link
+          href="static/plugins/scroll/jquery.mCustomScrollbar.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="static/plugins/font-awesome/css/all.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
