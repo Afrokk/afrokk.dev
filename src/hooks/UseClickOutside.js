6bc354e449e8ad18f3cf18af7c5309941d0680db
Afrokk <afrasiyabkhan379@gmail.com>
2023-04-28 13:11:42 -0400

    add hook

---
+++ src/hooks/UseClickOutside.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+let useClickOutside = (handler) => {
+  let domElement = useRef();
+
+  useEffect(() => {
+    let maybeHandler = (event) => {
+      if (
+        event.target.classList.contains("color_switch") ||
+        event.target.classList.contains("fa-moon") ||
+        domElement.current.contains(event.target)
+      ) {
+        return;
+      }
+      handler();
+    };
+
+    document.addEventListener("mousedown", maybeHandler);
+
+    return () => {
+      document.removeEventListener("mousedown", maybeHandler);
+    };
+  });
+
+  return domElement;
+};
+export default useClickOutside;
