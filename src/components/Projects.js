9ed6faf6e9c6b2d43b82176dbc2a941ebf91252e
Afrokk <afrasiyabkhan379@gmail.com>
2023-04-28 13:36:12 -0400

    tweak Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,6 +25,7 @@ const Projects = () => {
   const [hiddenProjects, setHiddenProjects] = useState(null);
   const [loading, setLoading] = useState(false);
   const [selectedProject, setSelectedProject] = useState(null);
+  const [showAlert, setShowAlert] = useState(true);
 
   const loadProjects = async () => {
     const query = `*[_type == "projects"] | order(created_at desc)`;
@@ -54,6 +55,10 @@ const Projects = () => {
     setPopup(true);
   };
 
+  const handleAlert = () => {
+    setShowAlert(false);
+  };
+
   return (
     <>
       <Popup
@@ -68,43 +73,59 @@ const Projects = () => {
         className="pp-section pp-scrollable section"
       >
         <div className="container">
-          <div className="title">
+          <div className="title project-section-title">
             <h3>Projects</h3>
+            {showAlert && (
+              <div className="alert alert-info d-block d-lg-none" role="alert">
+              <button
+                type="button"
+                className="close"
+                data-dismiss="alert"
+                onClick={handleAlert}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+              <i className="fas fa-info-circle mr-2"></i>
+              Click to view more details.
+            </div>
+            )}
           </div>
           <div className="row">
             {projects.map((project) => (
-              <div key={project._id} className="col-sm-6 m-15px-tb">
+              <div key={project._id} className="col-md-6 col-12 m-15px-tb">
                 <div
                   className="feature-box-01 media"
                   title="Click to view more details."
                   onClick={() => handlePopupClick(project)}
                 >
-                  <img
-                    src={urlFor(project.image).width(150).url()}
-                    alt={project.title}
-                    className="img-fluid"
-                  />
-                  <div className="feature-content media-body">
-                    <h5>{project.title}</h5>
-                    {project.tags ? (
-                      <>
-                        <div className="tag-cloud">
-                          {project.tags.slice(0, 3).map((tag, idx) => (
-                            <a key={idx} className="tag">
-                              {tag}
-                            </a>
-                          ))}
-                          {project.tags.length > 3 ? (
-                            <a className="tag">+{project.tags.length - 3}</a>
-                          ) : null}
-                        </div>
-                      </>
-                    ) : null}
-                    <p>
-                      {" "}
-                      {project.shortDescription.substring(0, 150)}
-                      {project.shortDescription.length > 150 && "..."}
-                    </p>
+                  <div className="media mx-auto my-auto flex-lg-row flex-column">
+                    <img
+                      src={urlFor(project.image).width(150).url()}
+                      alt={project.title}
+                      className="img-fluid mx-auto my-auto order-lg-1 order-1"
+                    />
+                    <div className="feature-content media-body order-lg-2 order-2">
+                      <h5>{project.title}</h5>
+                      {project.tags ? (
+                        <>
+                          <div className="tag-cloud">
+                            {project.tags.slice(0, 3).map((tag, idx) => (
+                              <a key={idx} className="tag">
+                                {tag}
+                              </a>
+                            ))}
+                            {project.tags.length > 3 ? (
+                              <a className="tag">+{project.tags.length - 3}</a>
+                            ) : null}
+                          </div>
+                        </>
+                      ) : null}
+                      <p>
+                        {" "}
+                        {project.shortDescription.substring(0, 150)}
+                        {project.shortDescription.length > 150 && "..."}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
