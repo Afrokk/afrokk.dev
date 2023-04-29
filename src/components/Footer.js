7c298a6f9a47d15bdbdff28048a6e6696d80fea9
Afrokk <afrasiyabkhan379@gmail.com>
2023-04-29 05:40:31 -0400

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="content">
+        <p>Developed with &#128153;</p>
+        <p>&copy; 2023 Afrokk Khan. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+export default Footer;
