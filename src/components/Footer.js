77021a47365ba8aef42e2cc8504e959b1e9edec2
Afrokk <afrasiyabkhan379@gmail.com>
2025-01-04 23:56:19 -0500

    feat(footer): add dynamic copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer">
       <div className="content">
         <p>Developed with &#128156;</p>
-        <p>&copy; 2023-2024 Afrokk Khan. All Rights Reserved.</p>
+        <p>&copy; 2023-{currentYear} Afrokk Khan. All Rights Reserved.</p>
       </div>
     </div>
   );
